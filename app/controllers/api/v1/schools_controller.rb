cfe444dc5f19c9a14f001b7bc56232445b604d0a
Freddy Rangel <frederick.rangel@gmail.com>
2014-07-17 13:47:07 -0500

    more advanced regex

--- app/controllers/api/v1/schools_controller.rb
+++ app/controllers/api/v1/schools_controller.rb
@@ -3,23 +3,49 @@ module Api
     class SchoolsController < ApplicationController
 
       def search_by_address
-        schools = School.nearby_schools(params[:address], 2).limit(30)
+        schools = School.nearby_schools(params[:address], 2)
         render json: schools, status: 200
       end
 
       def search_by_name_or_dbn
-        if params[:name_or_dbn].match(/ps\S/)
-          schools = School.search_by_name_or_dbn(params[:name_or_dbn].gsub(/ps\S/i) do |s|
-            s.insert(2, " 00")
-            s.insert(1, ".")
-          end
-          )
-          schools.limit(30)
-        else
-          schools = School.search_by_name_or_dbn(params[:name_or_dbn].gsub(/^ps/i, "p.s. ")).limit(30)
-        end
+        search_word = downcase_and_strip(params[:name_or_dbn])
+        search_word_with_spaces = add_spaces(search_word)
+        search_word_with_zeros = add_zeros(search_word_with_spaces)
+        original_search = School.search_by_name_or_dbn(params[:name_or_dbn])
+        schools = School.search_by_name_or_dbn(search_word_with_zeros)
 
+        render json: schools.concat(original_search), status: 200
         render json: schools, status: 200
+      end
+
+      private
+
+      def downcase_and_strip(phrase)
+        phrase.downcase.gsub(/[^a-z0-9\s]/i, '')
+      end
+
+      def add_spaces(phrase)
+        if phrase.match(/ps\S/ || /jsh\S/ || /ms\S/ || /is\S/)
+          return phrase.insert(2, " ")
+        elsif phrase.match(/jsh\s/)
+          return phrase.insert(3, " ")
+        else
+          return phrase
+        end
+      end
+
+      def add_zeros(phrase)
+        numbers = phrase.match(/ps (\d+)/ || /ms (\d+)/ || /is (\d+)/)
+        jsh_numbers = phrase.match(/jsh (\d+)/)
+        phrase = phrase.split[0] + " "
+        if numbers[1]
+          (3 - numbers[1].length).times { phrase = phrase + "0" }
+          phrase = phrase + numbers[1]
+        elsif jsh_numbers[1]
+          (3 - jsh_numbers[1].length).times { phrase = phrase + "0" }
+          phrase = phrase + jsh_numbers[1]
+        end
+        return phrase
       end
     end
   end
--- app/models/school.rb
+++ app/models/school.rb
@@ -3,6 +3,8 @@ class School < ActiveRecord::Base
   has_many :school_datas
   has_many :api_school_datas
 
+  before_save :downcase_and_strip
+
   include PgSearch
   pg_search_scope :search_by_name_or_dbn,
     against: [:name, :dbn],
@@ -10,12 +12,18 @@ class School < ActiveRecord::Base
       tsearch: { prefix: true }
     }
 
-  geocoded_by :full_street_address
-  after_validation :geocode, if: :primary_address_changed?
+  #geocoded_by :full_street_address
+  #after_validation :geocode, if: :primary_address_changed?
 
   scope :nearby_schools, ->(address, distance) { near(([address.to_s, "New York City"].compact.join(", ")), distance) }
 
   def full_street_address
     [primary_address, city, zip].compact.join(', ')
+  end
+
+  private
+
+  def downcase_and_strip
+    self.name = self.name.downcase.gsub(/[^a-z0-9\s]/i, '')
   end
 end
