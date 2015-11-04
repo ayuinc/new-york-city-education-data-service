1a9266770879acefc379057da6f33ed0fd679bcb
Joel Ibaceta <niohnex@gmail.com>
2015-11-04 12:25:29 -0500

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122183632) do
+ActiveRecord::Schema.define(version: 20151104171915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -794,6 +794,9 @@ ActiveRecord::Schema.define(version: 20141122183632) do
     t.float  "latitude"
     t.float  "longitude"
     t.string "dbn_id"
+    t.string "attendance_category"
+    t.string "quality_review"
+    t.string "closing_gap"
   end
 
 end
